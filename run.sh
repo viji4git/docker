bce0303351e4307474d9241fc90ea1da352f4497
root <root@ip-172-31-16-108.ap-south-1.compute.internal>
2018-04-09 05:56:48 +0000

    Making changes to the run.sh script

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 docker stop nginx
 docker rm nginx
+for images in `docker images | grep "none" | awk '{print $3}'`
+do
+   docker rmi $images
+done
 docker build -t nginx .
 docker run -tid --name nginx -p 80:80 nginx:latest
 docker ps
