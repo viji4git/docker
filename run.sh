2892367b7d6a1b55883d7ea79523240715a5084f
viji4git <40839415+viji4git@users.noreply.github.com>
2018-09-20 03:05:29 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ for images in `docker images | grep "none" | awk '{print $3}'`
 do
    docker rmi -f $images
 done
-docker build -t nginx:vallabh .
+docker build -t nginx:vallabh /opt/project/
 docker run -tid --name nginx -p 80:80 -p 8000:8000 --restart always nginx:vallabh
 docker ps
