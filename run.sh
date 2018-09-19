7767c468ad24f78ca43d479676a5ee275dde4e8e
root <root@ip-172-31-36-223.ec2.internal>
2018-09-19 18:40:14 +0000

    Deploy

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ docker stop nginx
 docker rm nginx
 for images in `docker images | grep "none" | awk '{print $3}'`
 do
-   docker rmi $images
+   docker rmi -f $images
 done
-docker build -t nginx .
-docker run -tid --name nginx -p 80:80 nginx:latest
+docker build -t nginx:vallabh .
+docker run -tid --name nginx -p 80:80 -p 8000:8000 --restart always nginx:vallabh
 docker ps
